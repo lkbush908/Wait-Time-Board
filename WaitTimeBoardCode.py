0588091464d5e7377668f500bd2055408cd15055
Logan Bush <lkbush908@gmail.com>
2026-06-19 09:11:15 -0400

    Updated incl no wait attractions

--- WaitTimeBoardCode.py
+++ WaitTimeBoardCode.py
@@ -1,6 +1,10 @@
 import requests
 import time
 from datetime import datetime
+import json
+
+currWait = 0
+Color = "Grey"
 
 
 #Attraction class for future use
@@ -35,7 +39,7 @@ try:
         print(current_time)
 
         #Response pull from themeparks.wiki wait time api
-        response = requests.get('https://api.themeparks.wiki/v1/entity/288747d1-8b4f-4a64-867e-ea7c9b27bad8/live')
+        response = requests.get('https://api.themeparks.wiki/v1/entity/7340550b-c14d-4def-80bb-acdb51d49a66/live')
 
         #Uncomment below  and comment response to run using template .json file
 
@@ -44,6 +48,9 @@ try:
 
         #Converts JSON version of response data into dictionary
         data = response.json()
+
+        with open("IOA.json", "w") as file:
+            json.dump(data, file)
 
         #Array for live data section of dictionary
         liveArray = data["liveData"]
@@ -60,15 +67,20 @@ try:
                 attrType = liveArray[i]["entityType"]
 
                 #pull queue data from curent attraction
-                queueData = liveArray[i]["queue"]
-                if queueData["STANDBY"]["waitTime"] is not None:
-                    currWait = queueData["STANDBY"]["waitTime"]
+                if "queue" in liveArray[i]:
+                    queueData = liveArray[i]["queue"]
+                    if queueData["STANDBY"]["waitTime"] is not None:
+                        currWait = queueData["STANDBY"]["waitTime"]
+                    else:
+                        currWait = "None"
+                    Color = colorAssign(queueData["STANDBY"]["waitTime"])
                 else:
                     currWait = "None"
+                    Color = "Grey"
 
                 #Assign variables for name and color
                 Name = liveArray[i]["name"]
-                Color = colorAssign(queueData["STANDBY"]["waitTime"])
+                
 
                 #Print Formatting
                 print(f"Name: {Name:<65} Type {attrType:<15} Wait Time: {currWait:<10} Color: {Color}")
@@ -85,7 +97,7 @@ except KeyboardInterrupt:
 
 # TO ADD
 # Create list/filter for wait time only attractions/shows
-# 
+# User Input for Park(?)
 # 
 # 
 # Color scale based on wait time
